ecddc11e3a8d3057f146f81334c7e398b0c6a254
manhhoang98 <110440251+manhhoang98@users.noreply.github.com>
2022-09-06 13:55:52 +0700

    ok

--- src/Thuchanhtinhlaisuat.java
+++ src/Thuchanhtinhlaisuat.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 public class Thuchanhtinhlaisuat {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("nhập số tiền gửi : ");
+        System.out.print("nhập số tiền gửi : ");
         double money = 1.0;
         money = scanner.nextDouble();
 
-        System.out.println("nhập số tháng gửi :" );
+        System.out.print("nhập số tháng gửi :" );
         double month = 1;
         month = scanner.nextDouble();
 
-        System.out.println("nhập lãi suất tháng");
+        System.out.print("nhập lãi suất tháng");
         double laisuat = 1.0;
         laisuat = scanner.nextDouble();
 
